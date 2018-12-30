a8305c3e01ebc447192d4931be26199999d244c9
mazahaka_tod <mazahaka_tod@mail.ru>
2018-12-30 20:29:39 +0300

    added project for integration tests

---
+++ DiplomMSSQLApp.IntegrationTests/OrganizationTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.IO;
+
+namespace DiplomMSSQLApp.IntegrationTests {
+    [TestFixture]
+    public class OrganizationTests : SeleniumTest {
+        public OrganizationTests() : base("DiplomMSSQLApp.WEB") { }
+
+        [Test]
+        public void SignInAsAdmin() {
+            ChromeDriver.Navigate().GoToUrl(GetAbsoluteUrl("/Organization/Index"));
+            SignIn();
+
+            Assert.AreEqual("Информация об организации", ChromeDriver.Title);
+        }
+
+        [Test]
+        public void EditPhoneOrganization() {
+            string phoneNumber = "+7 (922) " + new Random().Next(10000000, 99999999).ToString();
+
+            ChromeDriver.Navigate().GoToUrl(GetAbsoluteUrl("/Organization/Index"));
+            if (ChromeDriver.Title == "Вход") SignIn();
+            ChromeDriver.FindElement(By.PartialLinkText("Изменить информацию")).Click();
+            ChromeDriver.FindElement(By.Id("Phone")).Clear();
+            ChromeDriver.FindElement(By.Id("Phone")).SendKeys(phoneNumber);
+            ChromeDriver.FindElement(By.XPath("//input[@value='Сохранить']")).Click();
+
+            IWebElement element = ChromeDriver.FindElement(By.XPath("//dt[contains(.,'Телефон')]/following-sibling::dd"));
+            Assert.AreEqual(phoneNumber, element.Text);
+        }
+
+        [Test]
+        public void ExportDataToJsonFile() {
+            string fullPath = "./DiplomMSSQLApp.WEB/Results/Organizations.json";
+            if (File.Exists(fullPath)) File.Delete(fullPath);
+
+            ChromeDriver.Navigate().GoToUrl(GetAbsoluteUrl("/Organization/Index"));
+            if (ChromeDriver.Title == "Вход") SignIn();
+            ChromeDriver.FindElement(By.PartialLinkText("Экспортировать данные в JSON файл")).Click();
+
+            Assert.IsTrue(File.Exists(fullPath));
+        }
+    }
+}
---
+++ DiplomMSSQLApp.IntegrationTests/SeleniumTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace DiplomMSSQLApp.IntegrationTests {
+    [TestFixture]
+    public abstract class SeleniumTest {
+        const int iisPort = 50319;
+        private readonly string _applicationName;
+        private Process _iisProcess;
+
+        protected ChromeDriver ChromeDriver { get; set; }
+
+        protected SeleniumTest(string applicationName) {
+            _applicationName = applicationName;
+        }
+
+        [SetUp]
+        protected void RunBeforeEachTest() {
+            StartIIS();
+            ChromeDriver = new ChromeDriver(Path.Combine(Directory.GetCurrentDirectory(), "DiplomMSSQLApp.IntegrationTests"));
+        }
+
+        [TearDown]
+        protected void RunAfterEachTest() {
+            if (_iisProcess.HasExited == false) _iisProcess.Kill();
+            ChromeDriver.Quit();
+        }
+
+        private void StartIIS() {
+            string applicationPath = Path.Combine(Directory.GetCurrentDirectory(), _applicationName);
+            string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            _iisProcess = new Process();
+            _iisProcess.StartInfo.FileName = Path.Combine(programFiles, "IIS Express\\iisexpress.exe");
+            _iisProcess.StartInfo.Arguments = $"/path:{applicationPath} /port:{iisPort}";
+            _iisProcess.Start();
+        }
+
+        protected string GetAbsoluteUrl(string relativeUrl) {
+            if (!relativeUrl.StartsWith("/")) relativeUrl = "/" + relativeUrl;
+            return $"http://localhost:{iisPort}{relativeUrl}";
+        }
+
+        protected void SignIn() {
+            ChromeDriver.FindElement(By.Id("Name")).SendKeys("Admin");
+            ChromeDriver.FindElement(By.Id("Password")).SendKeys("MySecret");
+            ChromeDriver.FindElement(By.XPath("//input[@value='Войти']")).Click();
+        }
+    }
+}
